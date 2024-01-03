67fb7bb2873f1042800b1d1cdab8d14a5a61e93a
jsanteler <juliansanteler@tsn.at>
2024-01-04 00:55:42 +0100

    showCourseDetails und GetById

--- src/main/java/ui/Cli.java
+++ src/main/java/ui/Cli.java
@@ -6,6 +6,7 @@ import domain.Course;
 
 import java.sql.SQLOutput;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Cli {
@@ -32,6 +33,9 @@ public class Cli {
                 case "2":
                     showAllCourses();
                     break;
+                case "3":
+                    showCourseDetails();
+                    break;
                 case "x":
                     System.out.println("Auf Wiedersehen!");
                     break;
@@ -41,6 +45,25 @@ public class Cli {
             }
         }
         scan.close();
+    }
+
+    private void showCourseDetails() {
+        System.out.println("Für welchen Kurs möchten Sie die Kursdetails anzeigen?");
+        Long corseId = Long.parseLong(scan.nextLine());
+        try
+        {
+            Optional<Course> courseOptional = repo.getByID(corseId);
+            if(courseOptional.isPresent()){
+                System.out.println(courseOptional.get());
+            }else {
+                System.out.println("Kurs mit der ID " + corseId + " nicht gefunden!");
+            }
+
+        }catch (DatabaseException databaseException){
+            System.out.println("Datenbankfehler bei Kurs-Detailanzeige: " + databaseException.getMessage());
+        }catch (Exception exception){
+            System.out.println("Unbekannter Fehler der Kurs-Detailanzeige: " + exception.getMessage());
+        }
     }
 
     private void showAllCourses() {
@@ -68,7 +91,7 @@ public class Cli {
 
     private void showMenue(){
         System.out.println("---------------------- KURSMANAGEMENT --------------------");
-        System.out.println("(1) Kurs eingeben \t (2) ALle Kurse anzeigen \t");
+        System.out.println("(1) Kurs eingeben \t (2) ALle Kurse anzeigen \t" + "(3) Kursdetails anzeigen");
         System.out.println("(x) ENDE");
     }
 
