a3a6eed8fe99c770251949999d2b3f1ada821c87
jsanteler <juliansanteler@tsn.at>
2024-01-03 17:35:25 +0100

    UI erstellt

--- src/main/java/at/itkollegimst/Main.java
+++ src/main/java/at/itkollegimst/Main.java
@@ -1,6 +1,7 @@
 package at.itkollegimst;
 
 import dataaccess.MysqlDatabaseConnection;
+import ui.Cli;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -9,11 +10,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        Cli myCli = new Cli();
+        myCli.start();
+
         try {
             Connection myConnection =
                     MysqlDatabaseConnection.getConnection("jdbc:mysql://localhost:3306/kurssystem", "root", "1234");
             System.out.println("Verbindung aufgebaut");
-         
+
         }catch (ClassNotFoundException e){
             e.printStackTrace();
         }catch (SQLException e) {
---
+++ src/main/java/ui/Cli.java
@@ -0,0 +1,49 @@
+package ui;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Cli {
+
+    Scanner scan;
+
+    public Cli(){
+
+        this.scan = new Scanner(System.in);
+    }
+
+    public void start(){
+
+        String input = "-";
+        while (!input.equals("x")){
+            showMenue();
+            input = scan.nextLine();
+            switch (input){
+                case "1":
+                    System.out.println("Kurseingabe");
+                    break;
+                case "2":
+                    System.out.println("Alle Kurse anzeigen");
+                    break;
+                case "x":
+                    System.out.println("Auf Wiedersehen!");
+                    break;
+                default:
+                    inputError();
+                    break;
+            }
+        }
+        scan.close();
+    }
+
+
+    private void showMenue(){
+        System.out.println("---------------------- KURSMANAGEMENT --------------------");
+        System.out.println("(1) Kurs eingeben \t (2) ALle Kurse anzeigen \t");
+        System.out.println("(x) ENDE");
+    }
+
+    private void inputError(){
+        System.out.println("Bitte nur die Zahlen der Menüauswahl eingeben!");
+    }
+}
